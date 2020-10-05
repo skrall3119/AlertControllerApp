e7ce74dd67d4b5af7ca2924d8bbe258ab460e3fc
skrall3119 <alexjanci@yahoo.com>
2020-10-05 14:08:57 -0500

    init

--- AlertControllerApp/ViewController.swift
+++ AlertControllerApp/ViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func buttonTapped(_ sender: Any) {
+        let alert = UIAlertController(title: "Warning", message: "zombies are loose!", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: {action -> Void in
+        })
+        alert.addAction(okAction)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
